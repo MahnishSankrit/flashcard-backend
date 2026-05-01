9a5c52d0f0056b41483b41ee8d65d5218c19b469
Mahnish Sankrit <shortshub94@gmail.com>
2026-05-01 20:15:57 +0530

    Change upload method to single file for flashcards

--- routes/flashCards.route.js
+++ routes/flashCards.route.js
@@ -5,11 +5,13 @@ import { generateFlashcard } from "../controllers/flashcard.controller.js"
 
 const router = express.Router()
 
-router.post(
-  "/",
-  upload.fields([{ name: "file", maxCount: 1 }]),
-  generateFlashcard
-);
+// router.post(
+//   "/",
+//   upload.fields([{ name: "file", maxCount: 1 }]),
+//   generateFlashcard
+// );
+
+router.post("/", upload.single("file"), generateFlashcard);
 
 
 
